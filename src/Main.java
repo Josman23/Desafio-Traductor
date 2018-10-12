b752ae69cbc82fa336ba0dfbcdf313e8842d2fe6
Jose Manuel <josman_more23@hotmail.com>
2018-10-12 01:18:50 -0500

    Primera versión del traductor.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import traductor.Traductor;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa la cadena que deseas traducir.");
+
+        String cadena=scanner.nextLine();
+        Traductor traductor = new Traductor();
+        String traducir = traductor.Traductor(cadena);
+        System.out.println(traducir);
     }
 }
--- src/traductor/Traductor.java
+++ src/traductor/Traductor.java
@@ -2,9 +2,24 @@ package traductor;
 
 public class Traductor {
 
-    public String Traductor(String Cadena) {
+    public String Traductor(String cadena) {
 
+        String cadenaFinal="",
+        abecedario = "abcdefghijklmnñopqrstuvwxyz",
+        patronDeSustitucion = "ghijklmnñopqrstuvwxyzabcdef";
 
-        return null;
+        for(int i=1; i <= cadena.length(); i++){
+
+            String letra=cadena.substring(i-1,i);
+
+            if(letra.matches("[a-z]")){
+                cadenaFinal = cadenaFinal + abecedario.substring(patronDeSustitucion.indexOf(letra),patronDeSustitucion.indexOf(letra)+1);
+            }else if(letra.matches("[A-Z]")){
+                cadenaFinal = cadenaFinal + abecedario.toUpperCase().substring(patronDeSustitucion.toUpperCase().indexOf(letra),patronDeSustitucion.toUpperCase().indexOf(letra)+1);
+            }else{
+                cadenaFinal = cadenaFinal + letra;
+            }
+        }
+        return cadenaFinal;
     }
 }
